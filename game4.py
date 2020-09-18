06cab0c5bf260856f112a9273d1fd59a6edbfd22
sid-abc <siddhant.nigam@outlook.com>
2020-09-18 15:38:42 +0530

    game4

---
+++ game4.py
@@ -0,0 +1,48 @@
+######### KEYBOARD INPUT CONTROLS ##########
+
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and icon
+pygame.display.set_caption("Space invaders")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+# Game loop
+running = True
+while running:
+
+    # RGB = Red, Green, Blue
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # if keystroke is pressed check wether its right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.1
+        elif event.key == pygame.K_LEFT:
+            playerX_change = -0.1
+           
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+            playerX_change = 0                    
+    
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update()            
