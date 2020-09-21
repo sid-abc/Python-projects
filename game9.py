0e4acd9cac4969976ede356e4af77351b88c1ae4
sid-abc <siddhant.nigam@outlook.com>
2020-09-21 12:54:00 +0530

    game9

---
+++ game9.py
@@ -0,0 +1,106 @@
+######### Bullets ##########
+
+import pygame
+import random
+
+# Initialize the pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.png')
+
+# Title and icon
+pygame.display.set_caption("Space invaders")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('ufo.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 4
+enemyY_change = 40
+
+# Bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletY_change = 10
+bullet_state = "ready"
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))    
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+# Game loop
+running = True
+while running:
+
+    # RGB = Red, Green, Blue
+    screen.fill((0,0,0))
+    # Background image
+    screen.blit(background, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # if keystroke is pressed check wether its right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 5
+        elif event.key == pygame.K_LEFT:
+            playerX_change = -5
+
+        elif event.key == pygame.K_SPACE:
+            if bullet_state is "ready":
+                bulletX = playerX
+            fire_bullet(bulletX, bulletY) # bullet
+           
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+            playerX_change = 0                            
+    
+    playerX += playerX_change
+    # Boundries of player
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 4
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -4
+        enemyY += enemyY_change        
+    
+    # Bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+        
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
